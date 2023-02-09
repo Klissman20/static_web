561d63a61b582764ff1055e0227021119e317b11
Klissman20 <klesteban0.3@gmail.com>
2023-02-08 20:33:32 -0500

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: false,
-    port: 3005,
-    host: '0.0.0.0'
+    server: {
+    port: 3005 // default: 3000
+    }
 })
