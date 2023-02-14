1b3a019ff82cc3456f3afbb72f24f0e111b08a93
Klissman20 <klesteban0.3@gmail.com>
2023-02-14 17:36:19 -0500

    install tailwindcss dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: false,
-    css: ['@/assets/styles.css']
+    //css: ['@/assets/styles.css'],
+    css: ['~/assets/css/main.css'],
+    postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
