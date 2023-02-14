9fa8f005780f989b1b52eae643b2872b6b6ea850
Klissman20 <klesteban0.3@gmail.com>
2023-02-14 17:35:54 -0500

    Install tailwind dependencies

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,24 @@
 <template>
-    <div class="grid">
-        <strong>IOTizados.co!</strong> Muy pronto...
-    </div>
+  <div class="grid">
+    <strong>IOTizados.co!</strong> Muy pronto...
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
 </template>
 <style>
 html {
-    background-color: darkgray;
+  background-color: darkgray;
 }
 .grid {
-    display: grid;
-    background-color: white;
-    width: 50vw;
-    border: solid 1px black;
-    margin: auto;
-    border-radius: 20px;
-    text-align: center;
-    padding: 10vh;
-    font-size: 45px;
-    margin-top: 10vh;
-    box-shadow: 5px 5px 5px rgba(121, 118, 118, 0.38);
+  display: grid;
+  background-color: white;
+  width: 50vw;
+  border: solid 1px black;
+  margin: auto;
+  border-radius: 20px;
+  text-align: center;
+  padding: 10vh;
+  font-size: 45px;
+  margin-top: 10vh;
+  box-shadow: 5px 5px 5px rgba(121, 118, 118, 0.38);
 }
 </style>
